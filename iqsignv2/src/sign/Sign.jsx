bbd0eadf7ac9b4ff62e0a4dcc20acb9e2c979986
ibiyemisi64 <ibiyemisi_gbenebor@brown.edu>
2024-11-04 20:27:25 -0500

    edit code style

--- iqsignv2/src/sign/Sign.jsx
+++ iqsignv2/src/sign/Sign.jsx
@@ -1,66 +1,22 @@
 import React, { useState } from 'react';
-import TempSignImage from '../assets/cs2340-fall-background.png';
-import './Sign.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Stack from '@mui/material/Stack';
-import IconButton from '@mui/material/IconButton';
+import { IconButton, MenuItem, Select, InputLabel, FormControl, Typography, TextField, Stack } from '@mui/material';
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined';
 import FormatItalicIcon from '@mui/icons-material/FormatItalic';
 import FormatColorTextIcon from '@mui/icons-material/FormatColorText';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-
+import TempSignImage from '../assets/cs2340-fall-background.png';
+import './Sign.css'
 
 export function Sign() {
-  const [text, setText] = useState('Default Sign Text');
-
-  // Function to update the sign text
-  const updateText = (newText) => {
-    setText(newText);
-  };
 
   return (
-    <div className='sign-container' style={{
-      backgroundImage: `url(${TempSignImage})`,
-    }}>
-      {text}
+    <div className='sign-container'>
+      <Typography>Default Sign Text</Typography>
     </div>
   )
 }
 
-export function EditSign() {
-  const [text, setText] = useState('Default Sign Text');
-  const [isBold, setIsBold] = useState(false);
-  const [isItalic, setIsItalic] = useState(false);
-  const [isUnderline, setIsUnderline] = useState(false);
-
-  const handleTextChange = (event) => {
-    setText(event.target.value);
-  };
-
-  const handleBoldClick = () => {
-    setIsBold(!isBold);
-  };
-
-  const handleItalicClick = () => {
-    setIsItalic(!isItalic);
-  };
-
-  const handleUnderlineClick = () => {
-    setIsUnderline(!isUnderline);
-  };
-
-  const getTextStyle = () => {
-    let style = {};
-    if (isBold) style.fontWeight = 'bold';
-    if (isItalic) style.fontStyle = 'italic';
-    if (isUnderline) style.textDecoration = 'underline';
-    return style;
-  };
+export function SignTextFormatter() {
 
   const [font, setFont] = useState('Arial');
 
@@ -68,50 +24,62 @@ export function EditSign() {
     setFont(event.target.value);
   };
 
+  return (
+    <Stack spacing={2} direction="row">
+      <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
+        <InputLabel id="demo-select-small-label">Font</InputLabel>
+        <Select
+          labelId="demo-select-small-label"
+          id="demo-select-small"
+          label="Font"
+          value={font}
+          onChange={handleChange}
+        >
+          <MenuItem value="Arial">Arial</MenuItem>
+          <MenuItem value="Times New Roman" >Times New Roman </MenuItem>
+          <MenuItem value="Courier">Courier</MenuItem>
+          <MenuItem value="Bradley Hand">Bradley Hand</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        id="outlined-uncontrolled"
+        label="Size"
+        defaultValue="14"
+        size="small"
+        sx={{ m: 1, minWidth: 70, width: '70px' }}
+      />
+      <IconButton>
+        <FormatBoldIcon />
+      </IconButton>
+      <IconButton>
+        <FormatItalicIcon />
+      </IconButton>
+      <IconButton>
+        <FormatUnderlinedIcon />
+      </IconButton>
+      <IconButton>
+        <FormatColorTextIcon />
+      </IconButton>
+    </Stack>
+  );
+
+}
+export function EditSign() {
 
   return (
     <div
       className='formatting-container'
     >
-
-      <Stack spacing={2} direction="row">
-        <FormControl sx={{ m: 1, minWidth: 150 }} size="small">
-          <InputLabel id="demo-select-small-label">Font</InputLabel>
-          <Select
-            labelId="demo-select-small-label"
-            id="demo-select-small"
-            label="Font"
-            value={font}
-            onChange={handleChange}
-          >
-            <MenuItem value="Arial">Arial</MenuItem>
-            <MenuItem value="Times New Roman" >Times New Roman </MenuItem>
-            <MenuItem value="Courier">Courier</MenuItem>
-            <MenuItem value="Bradley Hand">Bradley Hand</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          id="outlined-uncontrolled"
-          label="Size"
-          defaultValue="14"
-          size="small"
-          sx={{ m: 1, minWidth: 70, width: '70px' }}
+      <SignTextFormatter />
+      <TextField
+          id="outlined-multiline-static"
+          label="Sign Text"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+          margin = "normal"
+          fullWidth
         />
-        <IconButton onClick={handleBoldClick}>
-          <FormatBoldIcon />
-        </IconButton>
-        <IconButton onClick={handleItalicClick}>
-          <FormatItalicIcon />
-        </IconButton>
-        <IconButton onClick={handleUnderlineClick}>
-          <FormatUnderlinedIcon />
-        </IconButton>
-        <IconButton>
-          <FormatColorTextIcon />
-        </IconButton>
-      </Stack>
-      <textarea className='formatting-edit-container' rows="auto" minrows="3" cols="auto" value={text} onChange={handleTextChange} style={getTextStyle()} />
-
     </div>
   );
 };
--- iqsignv2/src/toolbar/Toolbar.jsx
+++ iqsignv2/src/toolbar/Toolbar.jsx
@@ -1,60 +1,50 @@
 import * as React from 'react';
 import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import { ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import CollectionsIcon from '@mui/icons-material/Collections';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import LinkIcon from '@mui/icons-material/Link';
 import PersonIcon from '@mui/icons-material/Person';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Toolbar.css'
 
 function Toolbar() {
-  const [open, setOpen] = React.useState(true);
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
   return (
 
     <List
     className="list-group"
     >
       <ListItemButton>
-        <ListItemText primary="Current Status: Available" className="toolbarFont" />
+        <ListItemText primary="Current Status: Available" />
       </ListItemButton>
       <ListItemButton>
         <ListItemIcon>
           <EditIcon />
         </ListItemIcon>
-        <ListItemText primary="Edit" className="toolbarFont" />
+        <ListItemText primary="Edit" />
       </ListItemButton>
       <ListItemButton>
         <ListItemIcon>
           <CollectionsIcon />
         </ListItemIcon>
-        <ListItemText primary="Template Gallery" className="toolbarFont"/>
+        <ListItemText primary="Template Gallery"/>
       </ListItemButton>
       <ListItemButton>
         <ListItemIcon>
           <VisibilityIcon />
         </ListItemIcon>
-        <ListItemText primary="Preview" className="toolbarFont"/>
+        <ListItemText primary="Preview"/>
       </ListItemButton>
       <ListItemButton>
         <ListItemIcon>
           <LinkIcon />
         </ListItemIcon>
-        <ListItemText primary="Image Link" className="toolbarFont"/>
+        <ListItemText primary="Image Link"/>
       </ListItemButton>
       <ListItemButton>
         <ListItemIcon>
           <PersonIcon />
         </ListItemIcon>
-        <ListItemText primary="Profile" className="toolbarFont"/>
+        <ListItemText primary="Profile"/>
       </ListItemButton>
     </List>
   );
