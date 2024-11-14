3e928c0b3833c2d471e670d4b92e8aabed1abeb5
Michael Tu <michael_s_tu@brown.edu>
2024-11-14 15:02:04 -0500

    feat: map centers at current location
    - Added Geolocator permissions into `AndroidManifest.xml`,
    so the app should be able to get the current location in Debug Mode

--- flutter/alds/lib/mainpage.dart
+++ flutter/alds/lib/mainpage.dart
@@ -6,12 +6,14 @@
  */
 
 import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'widgets.dart' as widgets;
 import 'util.dart' as util;
+import 'locator.dart';
 
 class AldsMain extends StatelessWidget {
   const AldsMain({super.key});
@@ -34,13 +36,64 @@ class AldsMainWidget extends StatefulWidget {
 }
 
 class _AldsMainWidgetState extends State<AldsMainWidget> {
+  String _curLocationText = "";
+  Position? _curPosition;
+
   _AldsMainWidgetState();
 
   @override
   void initState() {
     super.initState();
     
-    // TODO: initial state code
+    // Initial state
+    Locator loc = Locator();
+    _curLocationText = loc.lastLocation ?? "N/A";
+    _getCurrentLocation();
+  }
+
+  _getCurrentLocation() async {
+    // Code adapted from: https://pub.dev/packages/geolocator#example
+
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the 
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale 
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately. 
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+    } 
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    final pos = await Geolocator.getCurrentPosition();
+    double lat = pos.latitude;
+    double long = pos.longitude;
+    util.log("CURRENT LOCATION: ($lat, $long)");
+    setState(() {
+      _curPosition = pos;
+    });
   }
 
   @override
@@ -56,7 +109,7 @@ class _AldsMainWidgetState extends State<AldsMainWidget> {
             height: 50,
             child: Center(
               child: Text(
-                "Current Location: ",
+                "Current Location: $_curLocationText",
                 style: TextStyle(color: Colors.black, fontSize: 20),
               ),
             ),
@@ -72,7 +125,6 @@ class _AldsMainWidgetState extends State<AldsMainWidget> {
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.blue,
-
                 ),
                 onPressed: _saveLocationAction, 
                 child: Text(
@@ -105,9 +157,11 @@ class _AldsMainWidgetState extends State<AldsMainWidget> {
   }
 
   Widget _createLocationMap() {
+    // Position? curPos = await Geolocator.getCurrentPosition()
+    //     .timeout(const Duration(seconds: 5));
     return FlutterMap(
       options: MapOptions(
-        initialCenter: LatLng(51.509364, -0.128928), // FIXME: Center over user's current location
+        initialCenter: (_curPosition != null) ? LatLng(_curPosition!.latitude, _curPosition!.longitude) : LatLng(51.509364, -0.128928),  // defaults to London
         initialZoom: 9.2,
       ),
       children: [
@@ -115,11 +169,24 @@ class _AldsMainWidgetState extends State<AldsMainWidget> {
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: "edu.brown.alds",  // FIXME: Fix the package name when you know it
         ),
+        CurrentLocationLayer(
+          alignPositionOnUpdate: AlignOnUpdate.always,
+          style: LocationMarkerStyle(
+            marker: const DefaultLocationMarker(
+              // child: Icon(
+              //   Icons.navigation,
+              //   color: Colors.white,
+              // ),
+            ),
+            markerSize: const Size(20, 20),
+            markerDirection: MarkerDirection.heading,
+          ),
+        ),
       ]
     );
   }
 
   void _saveLocationAction() {
-    
+    // TODO: Implement action for clicking the "Save Location" button
   }
 }
