8401791c19eb7432701f9309125993b346932433
“Shufan <she@iwu.edu>
2024-12-06 21:24:35 -0500

    Added forgot/reset password and modified routes, clickable but no backend, waiting for auth.

--- iqsignv2/src/App.jsx
+++ iqsignv2/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RegistrationPage from './Auth/RegistrationPage';
-import LoginPage from './Auth/LoginPage';
-import ProfilePage from './ProfilePage';
+import RegistrationPage from './Auth/RegistrationPage.jsx';
+import LoginPage from './Auth/LoginPage.jsx';
+import ProfilePage from './ProfilePage.jsx';
 import SignEditor  from './Sign/Sign.jsx';
 import Gallery from "./Gallery/Gallery.jsx";
 import Home from './HomePage';
+import ForgotPassword from './Auth/ForgotPassword.jsx'
+import ResetPassword from './Auth/ResetPassword.jsx'
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <Routes>
         <Route path="/register" element={<RegistrationPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgotpw" element={<ForgotPassword />} />
+        <Route path="/resetpw" element={<ResetPassword />} />
         <Route path="/" element={<LoginPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/edit" element={<SignEditor />} />
