f6fdd61ae9d562f273deb15fdbd031c09d6c5605
Jason Silva <littlejsilva@gmail.com>
2024-12-13 13:53:15 -0500

    gallery header comments

--- iqsignv2/src/Gallery/Gallery.jsx
+++ iqsignv2/src/Gallery/Gallery.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import {Box, Grid, IconButton, Stack} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 //import { create } from 'zustand';
-
 import {useGalleryStore} from "./hooks/galleryStore.jsx";
 import SignGallery from "./SubComponents/SignGallery.jsx";
 import TopBar from "../Topbar/TopBar.jsx";
+
+/*
+ *      gallery.jsx
+ *
+ *    Combine gallery sub components, served up as a route
+ *
+ */
+/*	Copyright 2024 Brown University -- Jason S. Silva		*/
+/// *******************************************************************************
+///  Copyright 2024, Brown University, Providence, RI.				 *
+///										 *
+///			  All Rights Reserved					 *
+///										 *
+///  Permission to use, copy, modify, and distribute this software and its	 *
+///  documentation for any purpose other than its incorporation into a		 *
+///  commercial product is hereby granted without fee, provided that the 	 *
+///  above copyright notice appear in all copies and that both that		 *
+///  copyright notice and this permission notice appear in supporting		 *
+///  documentation, and that the name of Brown University not be used in 	 *
+///  advertising or publicity pertaining to distribution of the software 	 *
+///  without specific, written prior permission. 				 *
+///										 *
+///  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS		 *
+///  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND		 *
+///  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY	 *
+///  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY 	 *
+///  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,		 *
+///  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS		 *
+///  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 	 *
+///  OF THIS SOFTWARE.								 *
+///										 *
+///******************************************************************************
+
+
+
 
 
 const Gallery = () => {
--- iqsignv2/src/Gallery/hooks/galleryStore.jsx
+++ iqsignv2/src/Gallery/hooks/galleryStore.jsx
@@ -4,6 +4,38 @@ import classImg from "../../assets/backgrounds/class.png"
 import oooImg from "../../assets/backgrounds/ooo-tomorrow.png"
 import fallImg from "../../assets/backgrounds/fall-background.png"
 import Cookies from "js-cookie"
+/*
+ *      galleryStore.jsx
+ *
+ *    handle all fetching logic, while providing a zustand store for other components,
+ *    mostly used in the gallery components
+ *
+ */
+/*	Copyright 2024 Brown University -- Jason S Silva.	*/
+/// *******************************************************************************
+///  Copyright 2024, Brown University, Providence, RI.				 *
+///										 *
+///			  All Rights Reserved					 *
+///										 *
+///  Permission to use, copy, modify, and distribute this software and its	 *
+///  documentation for any purpose other than its incorporation into a		 *
+///  commercial product is hereby granted without fee, provided that the 	 *
+///  above copyright notice appear in all copies and that both that		 *
+///  copyright notice and this permission notice appear in supporting		 *
+///  documentation, and that the name of Brown University not be used in 	 *
+///  advertising or publicity pertaining to distribution of the software 	 *
+///  without specific, written prior permission. 				 *
+///										 *
+///  BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS		 *
+///  SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND		 *
+///  FITNESS FOR ANY PARTICULAR PURPOSE.  IN NO EVENT SHALL BROWN UNIVERSITY	 *
+///  BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY 	 *
+///  DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,		 *
+///  WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS		 *
+///  ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 	 *
+///  OF THIS SOFTWARE.								 *
+///										 *
+///******************************************************************************
 
 
 export const useGalleryStore = create((set) => ({
