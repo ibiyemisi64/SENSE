c824b0da297b8afba6a3676c970bfd7c96ad55b0
“Shufan <she@iwu.edu>
2024-12-13 14:32:22 -0500

    added copyright statement to all files

--- iqsignv2/src/Auth/LoginPage.jsx
+++ iqsignv2/src/Auth/LoginPage.jsx
@@ -1,3 +1,31 @@
+/*
+ * LoginPage.jsx
+ *
+ * Purpose:
+ *
+ * Copyright 2024 Brown University --
+ *
+ * All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose other than its incorporation into a
+ * commercial product is hereby granted without fee, provided that the
+ * above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of Brown University not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.
+ *
+ * BROWN UNIVERSITY DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR ANY PARTICULAR PURPOSE. IN NO EVENT SHALL BROWN UNIVERSITY
+ * BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
+ * OF THIS SOFTWARE.
+ */
+
 import React, { useState, useEffect } from 'react';
 import { Container, TextField, Button, Typography, Link, Box, Checkbox, FormControlLabel } from '@mui/material';
 //import { hasher } from './utils/utils'; // our hasher
@@ -15,72 +43,7 @@ const LoginPage = () => {
   const [rememberMe, setRememberMe] = useState(false);
   const [error, setError] = useState('');
   const serverUrl = 'https://sherpa.cs.brown.edu:3336/rest';
-  //const serverUrl = 'http://csci2340.cs.brown.edu:3335'
 
-  {/**
-    const handleLogin = async (event) => {
-      event.preventDefault();
-      setError('');
-
-      try {
-        // retrieve padding and session info
-        const url = new URL(`${serverUrl}/login`);
-        const preLoginResponse = await axios.get(url.toString());
-
-        //if (!preLoginResponse.ok) {
-        //setError('Failed to initialize login.');
-        //return;
-        //}
-
-        const preLoginData = await preLoginResponse.data;
-        const {code: curPadding, session: curSession} = preLoginData;
-
-
-        // Hash credentials with padding and session
-        let lowerCaseUsername = username.toLowerCase();
-
-        let hashedPassword = hasher(password);
-        let paddedHash = hasher(hashedPassword + lowerCaseUsername);
-        let finalHash = hasher(paddedHash + curPadding);
-
-        // POST request with hashed credentials
-        const body = {
-          username: lowerCaseUsername,
-          padding: curPadding,
-          password: finalHash,
-          iqsignSession: curSession,
-        };
-
-        const loginResponse = await axios.post(`${serverUrl}/login`, body, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          credentials: 'include',
-          body: JSON.stringify(body),
-        });
-
-        const loginData = await loginResponse.data;
-
-        if (loginResponse.ok && loginData.status === 'OK') {
-
-          localStorage.setItem('iqsignSession', loginData.session);
-
-          // Redirect or display success
-          alert('Login successful!');
-          window.location.href = '/home';
-        } else if (loginData.TEMPORARY) {
-          alert('Temporary session detected. Redirecting to password reset...');
-          window.location.href = '/reset-password';
-        } else {
-          setError(loginData.error || 'Invalid login credentials.');
-        }
-      } catch (err) {
-        console.error('Login error:', err);
-        setError('An error occurred. Please try again.');
-      }
-    };
-  **/}
 
   // Redirect to /home if session cookie exists
   useEffect(() => {
